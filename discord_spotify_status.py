82b883cc599b0aabdaf866ed053446d8da9be16c
⚘'Ivan'⚘ <78805030+Evanechecssss@users.noreply.github.com>
2024-01-16 15:45:01 +0500

    0.5

--- discord_spotify_status.py
+++ discord_spotify_status.py
@@ -10,15 +10,15 @@ import exceptions
 DISCORD_SYNC_DELAY = 3
 DATA = dict()
 TRACKS = dict()
-DISPATCHER = None
 
 
 async def main():
     allow_sync = True
     tracks_data = {k: v for k, v in TRACKS.items()}
     last_sync_time = 0
-    access_token = (await DISPATCHER.get_access_token())['token']
-    client_token = await DISPATCHER.get_client_token((await DISPATCHER.get_access_token())['id'])
+    a_ = await dispatcher.get_access_token(DATA['sp_dc'])
+    access_token = a_['token']
+    client_token = await dispatcher.get_client_token(a_['id'])
     old_url = None
     previous_id = -1
     waiting_for_id = -1
@@ -27,12 +27,12 @@ async def main():
         if allow_sync:
             song = None
             try:
-                song = await DISPATCHER.get_current_playing_track(access_token)
+                song = await dispatcher.get_current_playing_track(access_token)
             except (ConnectionError, exceptions.AccessTokenExpired) as e:
                 await asyncio.sleep(5)
-                access_token = (await DISPATCHER.get_access_token())['token']
-                client_token = await DISPATCHER.get_client_token(
-                    (await DISPATCHER.get_access_token())['id'])
+                a = await dispatcher.get_access_token(DATA['sp_dc'])
+                access_token = a['token']
+                client_token = await dispatcher.get_client_token(a['id'])
                 print(e)
 
             if song is not None:
@@ -53,13 +53,13 @@ async def main():
 
                 if lyrics is None:
                     try:
-                        req = await DISPATCHER.get_lyric_request(access_token, client_token, url)
+                        req = await dispatcher.get_lyric_request(access_token, client_token, url)
                     except (ConnectionError, exceptions.AccessTokenExpired) as e:
                         await asyncio.sleep(5)
-                        access_token = (await DISPATCHER.get_access_token())['token']
-                        client_token = await DISPATCHER.get_client_token(
-                            (await DISPATCHER.get_access_token())['id'])
-                        req = await DISPATCHER.get_lyric_request(access_token, client_token, url)
+                        a = (await dispatcher.get_access_token(DATA['sp_dc']))
+                        access_token = a['token']
+                        client_token = await dispatcher.get_client_token(a['id'])
+                        req = await dispatcher.get_lyric_request(access_token, client_token, url)
                         print(e)
 
                     lyrics = lyrics_handler.simplify_lyrics(req)
@@ -91,9 +91,9 @@ async def main():
                     album_id = await tracks_methods.get_album_url(song)
                     if album_id in tracks_data:
                         song_status = tracks_data[album_id]
-                        await DISPATCHER.send_status(text=line_for_print, emoji=song_status[1])
+                        await dispatcher.send_status(text=line_for_print, emoji=song_status[1],discord_token=DATA['discord_token'])
                     else:
-                        await DISPATCHER.send_status(text=line_for_print, emoji=DATA['base_emoji'])
+                        await dispatcher.send_status(text=line_for_print, emoji=DATA['base_emoji'],discord_token=DATA['discord_token'])
         else:
             if time.perf_counter() - last_sync_time > DISCORD_SYNC_DELAY:
                 allow_sync = True
@@ -103,7 +103,6 @@ async def main():
 if __name__ == "__main__":
     DATA = json.load(open("data.json", encoding='utf-8'))
     TRACKS = json.load(open("tracks.json", encoding="utf-8"))
-    DISPATCHER = dispatcher.Dispatcher(DATA)
     print("Starting...")
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
